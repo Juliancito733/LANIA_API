c09b811fbf47d8f58779240490b2216687ac5694
Christian Julian Jimenez <zs22016079@estudiantes.uv.mx>
2025-05-15 23:00:45 -0600

    fix: corregir formato

--- src/candidatos/controllers/CandidatoController.php
+++ src/candidatos/controllers/CandidatoController.php
@@ -4,7 +4,8 @@ require_once __DIR__ . '/../services/CandidatoService.php';
 class CandidatoController {
     public static function index () {
         $candidatos = CandidatoService::getCandidatos();
-        header('Content-Type: application/json'); 
+        header('Content-Type: application/json; charset=utf-8');
+        
         echo json_encode($candidatos, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
     }
 }
